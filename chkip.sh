2831fe2b0b3bfc75ee13d869553f41079e7d2872
Robbie Cao <robbie.cao@gmail.com>
2016-09-07 09:28:08 +0800

    Fix string compare and add comments for how to setup
    Signed-off-by: Robbie Cao <robbie.cao@gmail.com>

--- chkip.sh
+++ chkip.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# setup:
+# $ sudo apt-get install mailutils lynx
+# $ crontab -e
+# # m h  dom mon dow  command
+# 0 */1 * * * /path/to/chkip.sh
+
+
 function exip() { lynx --dump http://ipecho.net/plain; }
 
 prev_ip=0
@@ -15,7 +22,7 @@ curr_ip=`exip`
 #echo $prev_ip
 #echo $curr_ip
 
-if [ $prev_ip!=$curr_ip ]; then
+if [ $prev_ip != $curr_ip ]; then
     mail -s "New IP: $curr_ip" user1@xxx.com user2@xxx.com < ip_addr
     echo $curr_ip > ip_addr
 fi
